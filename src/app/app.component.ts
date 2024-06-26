2ffa643aaf09e4c76b2727d16187ab030c531b7a
Anasutero <anamaria.sutero.10@gmail.com>
2024-06-26 14:15:53 -0300

    parei na estilização dos cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { FormsComponent } from './components/forms/forms.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { CardsListComponent } from './components/cards-list/cards-list.component';
-
+import { Location } from './types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
+import { CardComponent } from './components/card/card.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent,CommonModule,HttpClientModule, CardsListComponent],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent,CommonModule,HttpClientModule,CardsListComponent,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 }
@@ -19,8 +21,13 @@ import { CardsListComponent } from './components/cards-list/cards-list.component
 
 export class AppComponent {
   showList = new BehaviorSubject(false);
+  unitsList :   Location [] = [];
+
+  constructor(private unitService: GetUnitsService){}
+
+
   onSubmit(){
-    console.log("Chegou no app")
-    this.showList.next(true)
+    this.unitsList = this.unitService.getFilteredUnits();
+    this.showList.next(true);
   }
 }
--- src/app/components/cards-list/cards-list.component.ts
+++ src/app/components/cards-list/cards-list.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './cards-list.component.html',
   styleUrl: './cards-list.component.scss'
 })
 export class CardsListComponent implements OnInit{
+  @Input() unitsList:   Location[] = [];
 
 
   constructor() {}
 
-  ngOnInit(): void{
-  }
+  ngOnInit(): void{}
 }
